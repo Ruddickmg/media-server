76e46d68d9ab9517efaa7cbeb477790084607703
grant <ruddickmg@gmail.com>
2026-07-03 08:46:24 -1000

    make prowlarr use vpn

--- modules/services/prowlarr.nix
+++ modules/services/prowlarr.nix
@@ -7,6 +7,8 @@
 let
   inherit (lib) mkIf mkOption types;
   cfg = config.media-server.prowlarr;
+  vpnNs = config.media-server.vpn.namespace;
+  useVpn = cfg.vpnConfinement && config.media-server.vpn.enable;
   authXml = lib.optionalString config.media-server.security.enableAuthentication "<AuthenticationMethod>Forms</AuthenticationMethod>";
 in
 {
@@ -20,6 +22,11 @@ in
       type = types.bool;
       default = false;
       description = "Open ports in firewall for Prowlarr";
+    };
+    vpnConfinement = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Run Prowlarr inside the VPN network namespace";
     };
   };
 
@@ -63,6 +70,37 @@ in
         LockPersonality = true;
         RestrictNamespaces = true;
       };
+    }
+    // mkIf useVpn {
+      serviceConfig.NetworkNamespacePath = "/var/run/netns/${vpnNs}";
+    };
+
+    systemd.sockets.proxy-prowlarr = mkIf useVpn {
+      description = "Socket for proxy to Prowlarr in VPN namespace";
+      listenStreams = [ "9696" ];
+      wantedBy = [ "sockets.target" ];
+    };
+
+    systemd.services.proxy-prowlarr = mkIf useVpn {
+      description = "Proxy Prowlarr from VPN namespace to root namespace";
+      requires = [
+        "prowlarr.service"
+        "proxy-prowlarr.socket"
+      ];
+      after = [
+        "prowlarr.service"
+        "proxy-prowlarr.socket"
+      ];
+      unitConfig.JoinsNamespaceOf = "prowlarr.service";
+      serviceConfig = {
+        ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:9696";
+        PrivateNetwork = true;
+      };
+    };
+
+    networking.firewall = mkIf cfg.openFirewall {
+      allowedTCPPorts = [ 9696 ];
+      allowedUDPPorts = [ 9696 ];
     };
   };
 }
