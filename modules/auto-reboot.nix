c4e88e8b6eaf4fa75b109a5a0a10d8a537830625
grant <ruddickmg@gmail.com>
2026-07-04 11:56:31 -1000

    fix auto-reboot

--- modules/auto-reboot.nix
+++ modules/auto-reboot.nix
@@ -6,14 +6,10 @@
     script = ''
       set -euo pipefail
 
-      booted_kernel="$(${pkgs.coreutils}/bin/readlink -f /run/booted-system/kernel)"
-      built_kernel="$(${pkgs.coreutils}/bin/readlink -f /nix/var/nix/profiles/system/kernel)"
-      booted_initrd="$(${pkgs.coreutils}/bin/readlink -f /run/booted-system/initrd)"
-      built_initrd="$(${pkgs.coreutils}/bin/readlink -f /nix/var/nix/profiles/system/initrd)"
-      booted_modules="$(${pkgs.coreutils}/bin/readlink -f /run/booted-system/kernel-modules)"
-      built_modules="$(${pkgs.coreutils}/bin/readlink -f /nix/var/nix/profiles/system/kernel-modules)"
+      booted="$(${pkgs.coreutils}/bin/readlink /run/booted-system/{initrd,kernel,kernel-modules})"
+      built="$(${pkgs.coreutils}/bin/readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
 
-      if [ "$booted_kernel" != "$built_kernel" ] || [ "$booted_initrd" != "$built_initrd" ] || [ "$booted_modules" != "$built_modules" ]; then
+      if [ "$booted" != "$built" ]; then
         ${pkgs.systemd}/bin/shutdown -r +1 "Rebooting to apply kernel/initrd update"
       fi
     '';
@@ -23,7 +19,7 @@
     description = "Daily reboot check at 4am HST";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "*-*-* 04:00:00 Pacific/Honolulu";
+      OnCalendar = "*-*-* 04:00:00";
       Persistent = true;
     };
   };
