89e5df6ce0e6d03aed79b7d0a41d21b55d7c8201
grant <ruddickmg@gmail.com>
2026-07-05 22:27:11 -1000

    make serve happen post start for tailscale

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -29,35 +29,17 @@
       "network-online.target"
       "systemd-resolved.service"
     ];
-    serviceConfig.Environment = lib.mkAfter [
-      "TS_DEBUG_FIREWALL_MODE=nftables"
-    ];
-  };
-
-  systemd.services.tailscale-serve-paths = {
-    description = "Configure Tailscale Serve path-based HTTPS routing";
-    after = [
-      "tailscaled.service"
-      "network-online.target"
-    ];
-    wants = [
-      "tailscaled.service"
-      "network-online.target"
-    ];
-    wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      TimeoutStartSec = 30;
-      TimeoutStopSec = 10;
+      Environment = lib.mkAfter [
+        "TS_DEBUG_FIREWALL_MODE=nftables"
+      ];
+      ExecStartPost = [
+        "-${pkgs.tailscale}/bin/tailscale serve --set-path /prowlarr http://127.0.0.1:9696"
+        "-${pkgs.tailscale}/bin/tailscale serve --set-path /sonarr http://127.0.0.1:8989"
+        "-${pkgs.tailscale}/bin/tailscale serve --set-path /radarr http://127.0.0.1:7878"
+        "-${pkgs.tailscale}/bin/tailscale serve --set-path /lidarr http://127.0.0.1:8686"
+      ];
     };
-    script = ''
-      ${pkgs.tailscale}/bin/tailscale serve reset
-      ${pkgs.tailscale}/bin/tailscale serve --set-path /prowlarr http://127.0.0.1:9696
-      ${pkgs.tailscale}/bin/tailscale serve --set-path /sonarr http://127.0.0.1:8989
-      ${pkgs.tailscale}/bin/tailscale serve --set-path /radarr http://127.0.0.1:7878
-      ${pkgs.tailscale}/bin/tailscale serve --set-path /lidarr http://127.0.0.1:8686
-    '';
   };
 
   environment.systemPackages = [ pkgs.tailscale ];
