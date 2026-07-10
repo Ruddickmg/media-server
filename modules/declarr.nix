39bfbafc5849b9bcdbd6054c091aaa5c8da5bd0e
grant <ruddickmg@gmail.com>
2026-07-10 07:37:04 -1000

    fix: format files correctly for renaming

--- modules/declarr.nix
+++ modules/declarr.nix
@@ -44,6 +44,14 @@ let
           enableCompletedDownloadHandling = true;
           copyUsingHardlinks = true;
         };
+        naming = {
+          renameEpisodes = true;
+          standardEpisodeFormat = "{Series Title} - S{season:00}E{episode:00} - {Episode Title} - {Quality Full}";
+          dailyEpisodeFormat = "{Series Title} - {Air-Date} - {Episode Title} - {Quality Full}";
+          animeEpisodeFormat = "{Series Title} - S{season:00}E{episode:00} - {Episode Title} - {Quality Full}";
+          seriesFolderFormat = "{Series Title}";
+          seasonFolderFormat = "Season {season:00}";
+        };
       };
 
       qualityProfile = { };
@@ -82,6 +90,11 @@ let
           enableCompletedDownloadHandling = true;
           copyUsingHardlinks = true;
         };
+        naming = {
+          renameMovies = true;
+          standardMovieFormat = "{Movie CleanTitle} ({Release Year}) - {Quality Full}";
+          movieFolderFormat = "{Movie CleanTitle} ({Release Year})";
+        };
       };
 
       qualityProfile = { };
@@ -119,6 +132,12 @@ let
         mediamanagement = {
           enableCompletedDownloadHandling = true;
           copyUsingHardlinks = true;
+        };
+        naming = {
+          renameTracks = true;
+          standardTrackFormat = "{Artist Name} - {Album Title} - {track:00} - {Track Title}";
+          albumFolderFormat = "{Artist Name} - {Album Title} ({Release Year})";
+          artistFolderFormat = "{Artist Name}";
         };
       };
 
