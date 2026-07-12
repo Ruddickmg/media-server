82cff98a29da19ac85e3cd2f7b96822fd4633b8b
grant <ruddickmg@gmail.com>
2026-07-12 13:08:39 -1000

    remove line break

--- modules/common.nix
+++ modules/common.nix
@@ -221,6 +221,9 @@ in
     programs.starship = {
       enable = true;
       settings.add_newline = false;
+      line_break = {
+        disabled = true;
+      };
     };
 
     programs.zsh.interactiveShellInit = ''
